faf65fcb98e2260ee99bb248ef6b282c99daf187
lequangcanh <lequangcanh94@gmail.com>
2016-11-24 10:32:37 +0700

    Service for Profile

--- src/app/components/profile/controllers/profileController.js
+++ src/app/components/profile/controllers/profileController.js
@@ -1,15 +1,24 @@
 'use strict';
 
-angular.module('beertube.profile').controller('ProfileCtrl', ['$scope', 'profile', '$routeParams',
-  function ($scope, profile, $routeParams) {
-  	$scope.userIdParam = $routeParams.userId;
-  	$scope.getUser = function (id) {
-  	  profile.getUserById(id).then(function (response) {
-  	    $scope.user = response.data;
-  	  });
-  	};
-  	$scope.getUser($scope.userIdParam);
-    $scope.updateUser = function () {
+angular.module('beertube.profile').controller('ProfileCtrl', ['$scope', 'profile', '$cookies', '$window',
+  function ($scope, profile, $cookies, $window) {
+    var userInfo = JSON.parse($cookies.get('user_info'));
+    $scope.user = {
+      email: userInfo.email,
+      fullName: userInfo.fullName,
+      avatarUrl: userInfo.avatarUrl
+    };
+    $scope.sUpdate = true;
+    $scope.fUpdate = true;
 
+    $scope.updateUser = function () {
+      profile.updateUser($scope.user).then(function (response) {
+        var newUserInfo = JSON.stringify(response.data.data);
+        $cookies.remove('user_info');
+        $cookies.put('user_info', newUserInfo);
+        $scope.sUpdate = false;
+      }, function (response) {
+        $scope.fUpdate = false;
+      });
     };
   }]);
--- src/app/components/profile/services/profileService.js
+++ src/app/components/profile/services/profileService.js
@@ -1,8 +1,13 @@
 'use strict';
 
-angular.module('beertube.profile').service('profile', ['$http', 
-  function ($http) {
-    this.getUserById = function (userId) {
-      return $http.get("http://webservice-dinhphan.rhcloud.com/rest/user/" + userId);
+angular.module('beertube.profile').service('profile', ['$http', '$cookies',
+  function ($http, $cookies) {
+    this.updateUser = function (user) {
+      var token = $cookies.get('user_token');
+      var config = {
+        headers: {'Content-Type': 'application/json',
+                  'Authorization': token}
+      };
+      return $http.put("http://videoservice-dinhphan.rhcloud.com/users/update", user, config);
     };
   }]);
