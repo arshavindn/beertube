ce5dafaabf1d86ce4b605d78b43bae0fe6e16b89
lequangcanh <lequangcanh94@gmail.com>
2016-11-24 15:26:43 +0700

    Service for upload video

--- src/app/components/upload/controllers/uploadController.js
+++ src/app/components/upload/controllers/uploadController.js
@@ -1,21 +1,39 @@
 'use strict';
 
-angular.module('beertube.upload').controller('UploadCtrl', ['$scope',
-  function ($scope) {
+angular.module('beertube.upload').controller('UploadCtrl', ['$scope', 'upload',
+  function ($scope, upload) {
     $scope.video = {
-      videoId: 0,
       videoName: "",
+      category: {categoryId: ""},
       videoUrl: "",
-      videoDes: "",
-      videoImageUrl: ""
+      description: "",
+      videoImage: ""
     }
-    $scope.videoLink = "";
-    $scope.uploadVideo = function () {
+    $scope.fVideo = true;
+    $scope.sVideo = true;
+    $scope.categories = {};
 
-    }
+    upload.getAllCategories().then(function(response){
+      var data_arr = response.data.data;
+      for(var i=0; i<data_arr.length; i++) {
+        var name = data_arr[i].categoryName;
+        var id = data_arr[i].categoryId;
+        $scope.categories[name] = id;
+      }
+    }, function(response){
+
+    });
     $scope.viewDemo = function () {
       var str = $scope.videoLink.split("watch?v=");
       $scope.video.videoUrl = "https://www.youtube.com/embed/" + str[1];
-      $scope.video.videoImageUrl = "http://img.youtube.com/vi/" + str[1] + "/mqdefault.jpg";
+      $scope.video.videoImage = "http://img.youtube.com/vi/" + str[1] + "/mqdefault.jpg";
+    }
+
+    $scope.uploadVideo = function () {
+      upload.createVideo($scope.video).then(function(response){
+        $scope.sVideo = false;
+      }, function(response){
+        $scope.fVideo = false;
+      });
     }
   }]);
--- src/app/components/upload/services/uploadService.js
+++ src/app/components/upload/services/uploadService.js
@@ -1,3 +1,21 @@
 'use strict';
 
-angular.module('beertube.upload').service('upload', []);
+angular.module('beertube.upload').service('upload', ['$http', '$cookies', 
+  function($http, $cookies) {
+  	this.createVideo = function(video) {
+  	  var token = $cookies.get('user_token');
+      var config = {
+        headers: {'Content-Type': 'application/json',
+                  'Authorization': token}
+      };
+      return $http.post('http://videoservice-dinhphan.rhcloud.com/videos', video, config);
+  	};
+
+  	this.getAllCategories = function() {
+  	  var config = {
+  	    headers: {'Content-Type': 'application/json'}
+  	  };
+  	  return $http.get('http://videoservice-dinhphan.rhcloud.com/categories', config);
+  	};
+  }
+]);
